361ebe0a70382cbbeef4d3557776e7d9a44aef31
bav <vladimir.bavaev@gmail.com>
2022-03-12 21:29:51 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: _MyHomePage(),
+    );
+  }
+}
+
+class _MyHomePage extends StatelessWidget {
+  final Widget svgIcon1 = SvgPicture.asset(
+    'assets/images/image.svg',
+    height: 300,
+    width: 200,
+  );
+
+  final Widget svgIcon2 = SvgPicture.network(
+    'https://raw.githubusercontent.com/dnfield/flutter_svg/7d374d7107561cbd906d7c0ca26fef02cc01e7c8/example/assets/flutter_logo.svg',
+    height: 300,
+    width: 200,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+              items: [
+                svgIcon1,
+                svgIcon2,
+                Image.asset('assets/images/1.png'),
+                Image.asset('assets/images/2.png')
+              ],
+              options: CarouselOptions(
+                autoPlay: false,
+                enlargeCenterPage: true,
+                viewportFraction: 0.9,
+                aspectRatio: 2.0,
+                initialPage: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
